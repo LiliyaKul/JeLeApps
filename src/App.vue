61c302405ca08cb6f13c21d0e7e268434a586cc8
Liliya K <Lilyusha-kul@yandex.ru>
2025-03-13 22:19:05 +0300

    Black square

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <div class="square"></div>
+  </div>
+</template>
+
+<style>
+  .square {
+    width: 200px;
+    height: 200px;
+    background-color: black;
+    padding: 0px;
+  }
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
